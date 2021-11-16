32efadfbcd28189d786fbc9bd711ba8a4dd90806
mananmehta22 <manan.mehta@sts.in>
2021-11-16 17:58:55 +0530

    relationship added to model

--- employee/models/emp_info.py
+++ employee/models/emp_info.py
@@ -3,9 +3,8 @@ import decimal
 import json
 from flask.globals import request
 
-from sqlalchemy.sql.sqltypes import CHAR, VARCHAR, Date
+from sqlalchemy.sql.sqltypes import CHAR, INTEGER, VARCHAR, Date
 
-from owsresponse import response
 from sqlalchemy import case
 from sqlalchemy import Column
 from sqlalchemy import DATETIME
@@ -28,15 +27,23 @@ class Employees(mysql.BaseModel):
     last_name = Column(VARCHAR(20), nullable=False)
     birth_date = Column(Date(10), nullable=False)
     gender = Column(Enum('M','F'))
+    hire_date = Column(Date(10))
     last_updated = Column(DATETIME)
+    
+    
+    children_1 = relationship("Salaries", back_populates="parent_1")
+    children_2 = relationship("Titles", back_populates="parent_2")
+    children_3 = relationship("Dept_Manager", back_populates="parent_3")
+    children_4 = relationship("Leaves", back_populates="parent_4")
 
 
-    def __init__(self, emp_no, first_name, last_name, birth_date, gender, last_updated):
+    def __init__(self, emp_no, first_name, last_name, birth_date, gender, hire_date, last_updated):
         self.emp_no = emp_no
         self.first_name = first_name
         self.last_name = last_name
         self.birth_date = birth_date
         self.gender = gender
+        self.hire_date = hire_date
         self.last_updated = last_updated
 
 
@@ -45,10 +52,13 @@ class Salaries(mysql.BaseModel):
     __tablename__ = 'salaries'
 
 
-    emp_no = Column(Integer(11), primary_key=True, nullable=False)
+    emp_no = Column(Integer(11), primary_key=True, nullable=False, ForeignKey('employees.emp_no'))
     salary = Column(Integer(11),primary_key=True, nullable=False)
     from_date = Column(Date(), nullable=False)
     to_date = Column(Date(), nullable=False)
+
+
+    parent_1 = relationship("Employees", back_populates="children_1")
 
 
     def __init__(self, emp_no, salary, from_date, to_date):
@@ -62,10 +72,12 @@ class Titles(mysql.BaseModel):
     __tablename__ = 'titles'
 
 
-    emp_no = Column(Integer(11), primary_key=True, nullable=False)
+    emp_no = Column(Integer(11), primary_key=True, nullable=False, ForeignKey('employees.emp_no'))
     title = Column(VARCHAR(50),primary_key=True, nullable=False)
-    from_date = Column(Date(),primary_key=True, nullable=False)
+    from_date = Column(Date() nullable=False)
     to_date = Column(Date(), nullable=False)
+
+    parent_2 = relationship("Employees", back_populates="children_2")
 
 
     def __init__(self, emp_no, title, from_date, to_date):
@@ -79,10 +91,14 @@ class Dept_emp(mysql.BaseModel):
     __tablename__ = 'dept_emp'
 
 
-    emp_no = Column(Integer(11), primary_key=True, nullable=False)
-    dept_no = Column(CHAR(4),primary_key=True, nullable=False)
+    emp_no = Column(Integer(11), primary_key=True, nullable=False, ForeignKey('employees.emp_no'))
+    dept_no = Column(CHAR(4),primary_key=True, nullable=False, ForeignKey('departments.dept_no'))
     from_date = Column(Date(), nullable=False)
     to_date = Column(Date(), nullable=False)
+
+
+parent_3 = relationship("Employees", back_populates="children_3")
+parent_5 = relationship("Departments", back_populates="children_4")
 
 
     def __init__(self, emp_no, dept_no, from_date, to_date):
@@ -96,24 +112,32 @@ class Dept_Manager(mysql.BaseModel):
     __tablename__ = 'dept_manager'
 
 
-    dept_no = Column(CHAR(4), primary_key=True, nullable=False)
+    manager_id = Column(Integer(11), primary_key=True, nullable=False)
+    dept_no = Column(CHAR(4), primary_key=True, nullable=False, ForeignKey('departments.dept_no'))
     emp_no = Column(Integer(11),primary_key=True, nullable=False)
     from_date = Column(Date(20), nullable=False)
     to_date = Column(Date(10), nullable=False)
 
+    parent_6 = relationship("Departments", back_populates="children_5")
 
-    def __init__(self, emp_no, salary, from_date, to_date):
+
+    def __init__(self, manager_id, emp_no, dept_no, from_date, to_date):
+        self.manager_id = manager_id
         self.emp_no = emp_no
-        self.salary = salary
+        self.dept_no = dept_no
         self.from_date = from_date
-        self.to = to_date
+        self.to_date = to_date
 
 class Departments(mysql.BaseModel):
 
     __tablename__ = 'departments'
 
     dept_no = Column(CHAR(4), primary_key=True, nullable=False)
-    dept_name = Column(Integer(11), nullable=False)
+    dept_name = Column(VARCHAR(11), nullable=False)
+
+    children_5 = relationship("Dept_emp", back_populates="parent_4")
+    children_6 = relationship("Dept_Manager", back_populates="parent_5")
+    children_7 = relationship("Leaves", back_populates="parent_6")
 
 
     def __init__(self, dept_no, dept_name):
@@ -124,16 +148,23 @@ class Leaves(mysql.BaseModel):
 
     __tablename__ = 'leaves'
 
-    emp_no = Column(Integer(11), primary_key=True, nullable=False)
-    dept_no = Column(CHAR(4),primary_key=True, nullable=False)
-    leaves_taken = Column(Integer(2), primary_key=True)
-    leaves_left = Column(Integer(2), primary_key=True)
+    emp_no = Column(Integer(11), primary_key=True, nullable=False), ForeignKey('employees.emp_no')
+    dept_no = Column(CHAR(4),primary_key=True, nullable=False, ForeignKey('departments.dept_no'))
+    leaves_taken = Column(Integer(2))
+    leaves_left = Column(Integer(2))
+    leaves_without_pay = Column(Integer(2), primary_key=True)
 
-    def __init__(self, emp_no, dept_no, leaves_taken, leaves_left):
+
+    parent_4 = relationship("Employees", back_populates="children_4")    
+    parent_7 = relationship("Departments", back_populates="children_6")
+
+
+    def __init__(self, emp_no, dept_no, leaves_taken, leaves_left, leaves_without_pay):
         self.emp_no = emp_no
         self.dept_no = dept_no
         self.leaves_taken = leaves_taken
         self.leaves_left = leaves_left
+        self.leaves_without_pay = leaves_without_pay
 
 @mysql.wrap_db_errors
 def get_emp(emp_id):
